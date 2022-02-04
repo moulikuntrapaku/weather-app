f2e3926ed788b01774ed1234963823b4966e0a30
moulikuntrapaku <moulikuntrapaku@gmail.com>
2022-02-04 12:17:15 +0530

    weather app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import Axios from "axios";
+import CityComponent from "./modules/CityComponent";
+import WeatherComponent from "./modules/WeatherInfoComponent";
+
+export const WeatherIcons = {
+  "01d": "/react-weather-app/icons/sunny.svg",
+  "01n": "/react-weather-app/icons/night.svg",
+  "02d": "/react-weather-app/icons/day.svg",
+  "02n": "/react-weather-app/icons/cloudy-night.svg",
+  "03d": "/react-weather-app/icons/cloudy.svg",
+  "03n": "/react-weather-app/icons/cloudy.svg",
+  "04d": "/react-weather-app/icons/perfect-day.svg",
+  "04n": "/react-weather-app/icons/cloudy-night.svg",
+  "09d": "/react-weather-app/icons/rain.svg",
+  "09n": "/react-weather-app/icons/rain-night.svg",
+  "10d": "/react-weather-app/icons/rain.svg",
+  "10n": "/react-weather-app/icons/rain-night.svg",
+  "11d": "/react-weather-app/icons/storm.svg",
+  "11n": "/react-weather-app/icons/storm.svg",
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 380px;
+  padding: 20px 10px;
+  margin: auto;
+  border-radius: 4px;
+  box-shadow: 0 3px 6px 0 #555;
+  background: white;
+  font-family: Montserrat;
+`;
+
+const AppLabel = styled.span`
+  color: black;
+  margin: 20px auto;
+  font-size: 18px;
+  font-weight: bold;
+`;
 
 function App() {
+  const [city, updateCity] = useState();
+  const [weather, updateWeather] = useState();
+  const fetchWeather = async (e) => {
+    e.preventDefault();
+    const response = await Axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fd0e76bbb8817254aeb0cec22a3fb6c3`,
+    );
+    updateWeather(response.data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <AppLabel>Mouli's Weather App</AppLabel>
+      {city && weather ? (
+        <WeatherComponent weather={weather} city={city} />
+      ) : (
+        <CityComponent updateCity={updateCity} fetchWeather={fetchWeather} />
+      )}
+    </Container>
   );
 }
 
